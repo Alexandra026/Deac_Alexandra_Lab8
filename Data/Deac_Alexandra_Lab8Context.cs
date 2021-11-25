66e01abb5e6e1dabb7ec28d00fb8a7d6756120b6
Alexandra Ioana Deac <Deac.Gh.Alexandra@student.utcluj.ro>
2021-11-25 16:11:59 +0200

    Add project files.

---
+++ Data/Deac_Alexandra_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Deac_Alexandra_Lab8.Models;
+
+namespace Deac_Alexandra_Lab8.Data
+{
+    public class Deac_Alexandra_Lab8Context : DbContext
+    {
+        public Deac_Alexandra_Lab8Context (DbContextOptions<Deac_Alexandra_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Deac_Alexandra_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Deac_Alexandra_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
